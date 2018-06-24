8fc79caa15e0c998f5fe4e70a42a1f8cd947b75c
Denis Prokhorchik <dionis_999@hotmail.com>
2018-06-24 22:12:02 +0300

    added script DDL - iaccount - dbo.User.sql

---
+++ source/ao.i-account.db/Sctipts/DDL - iaccount - dbo.User.sql
@@ -0,0 +1,11 @@
+﻿if object_id('dbo.User', 'U') is not null
+	drop table dbo.[User] 
+
+create table dbo.[User]
+(
+	user_id int not null identity(1,1),
+	username NVARCHAR(100) not null,
+	pwd NVARCHAR(100) not null,
+	constraint PK_User primary key (user_id)	
+)
+go
