5d3e94c1e6245dfa87a77cfb41f0889eb5ba9804
Denis Prokhorchik <dionis_999@hotmail.com>
2018-06-24 22:17:17 +0300

    added script DDL - iaccount - dbo.Contact

---
+++ source/ao.i-account.db/Sctipts/DDL - iaccount - dbo.Contact.sql
@@ -0,0 +1,10 @@
+﻿if object_id('dbo.Contact', 'U') is not null
+	drop table dbo.Contact 
+
+create table dbo.Contact
+(
+	contact_id int not null identity(1,1),
+	user_id int not null,
+	constraint PK_Contact primary key (contact_id)	
+)
+go
